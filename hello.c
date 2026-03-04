1ae8d2efdcedd9cc0d33c28f2823b525e9a888aa
Sung-Dong <sdkim@hansung.ac.kr>
2026-03-04 13:51:49 +0900

    Initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+
+    printf("Hello, World!\n");
+    printf("The value of a is: %d\n", a++);
+    printf("The value of a after post-increment is: %d\n", a);
+    return 0;
+}
