e0b0ff6a8ff8b2bc357034da6725a551f6414b02
Sung-Dong <sdkim@hansung.ac.kr>
2026-03-04 14:32:42 +0900

    add hello2.c

---
+++ hello2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+
+    printf("Hello, World again!\n");
+    printf("The value of a is: %d\n", a++);
+    printf("The value of a after post-increment is: %d\n", a);
+    return 0;
+}
